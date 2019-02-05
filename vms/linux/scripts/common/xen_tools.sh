937d2460ab67c44f3175d5be9f5ed1831d3410fa
David Estes <davydotcom@gmail.com>
2019-02-05 10:47:47 -0500

    installing more base tools

---
+++ vms/linux/scripts/common/xen_tools.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+set -e
+. /tmp/os_detect.sh
+
+
+case "$OS_RELEASE" in
+  ubuntu)
+	apt-get -y install xen-tools
+        ;;
+
+  centos|rhel|ol)
+	yum install -y debootstrap perl-Text-Templateh \
+             perl-Config-IniFiles perl-File-Slurp \
+             perl-File-Which perl-Data-Dumper
+  	yum install qemu-guest-agent -y
+  	cd /tmp
+  	wget http://xen-tools.org/software/xen-tools/xen-tools-4.3.1.tar.gz
+  	tar zxvf xen-tools-4.3.1.tar.gz
+	cd xen-tools-4.3.1
+	make install
+   	;;
+
+  *)
+   	;;
+esac
+
