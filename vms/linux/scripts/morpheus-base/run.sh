6e1494b5876c54942cf86a11f9dd31253ad3c1e9
David Estes <davydotcom@gmail.com>
2020-01-25 12:36:22 -0500

    Added Hyper-V images for ubuntu 18

--- vms/linux/scripts/morpheus-base/run.sh
+++ vms/linux/scripts/morpheus-base/run.sh
@@ -1,10 +1,7 @@
 #!/bin/bash -eu
 . /tmp/os_detect.sh
 
-if [ -z "$GIT_HASH" ]; then
-  echo "No GIT_HASH was set in environment, this should be set in Packer."
-  exit 1
-fi
+
 
 if [ -z "$BUILD_NAME" ]; then
   echo "No BUILD_NAME was set in environment, this should be set in Packer."
@@ -14,7 +11,7 @@ else
 	  BUILD_TIME=$(date -u +%FT%T%z)
 	fi
 	# sudo is not needed since the script is being called using sudo
-	echo "$GIT_HASH $BUILD_NAME $BUILD_TIME" > /root/morpheus.version
+	echo "$BUILD_NAME $BUILD_TIME" > /root/morpheus.version
 	chmod 644 /root/morpheus.version
 
 	PACKAGES="git wget curl vim unzip"
