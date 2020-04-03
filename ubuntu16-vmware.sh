f2124c00f56b47464c92149bafe28c6fac99bcfd
David Estes <davydotcom@gmail.com>
2020-04-03 12:40:22 -0400

    fixing some scripts

---
+++ ubuntu16-vmware.sh
@@ -0,0 +1,1 @@
+./morpheus-library -base=vms/linux -template vms/linux/templates/centos/centos-7/centos-base.json -var-file vms/linux/templates/centos/centos-7/centos-7_5-x86_64.json -sudo -var "git_hash=$GIT_HASH" -only=kvm
