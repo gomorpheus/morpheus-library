47446b0728748da50ca9564fa971628bcd1862b4
bdwheeler <bdwheeler@gmail.com>
2022-03-31 11:59:04 -0600

    adding agent container

---
+++ containers/morpheus/agent/morpheus-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+JAVA_STARTUP="-jar agent.jar"
+
+if [ "$1" = 'java' ]; then
+	# if second arg is base 64 encoded config
+	if [ -z "$AGENT_CONFIG" ]; then
+		if [ "$2" ]; then
+    		JAVA_STARTUP="$JAVA_STARTUP -j $2"
+    		echo $JAVA_STARTUP
+		fi
+	else
+		JAVA_STARTUP="$JAVA_STARTUP $AGENT_CONFIG"
+	fi
+	java -Xms$JVM_MIN_MEM -Xmx$JVM_MAX_MEM $JAVA_STARTUP 
+fi
+
+exec "$@"
